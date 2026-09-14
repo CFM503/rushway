4114e3c1fbe6a764cdfdc9e52c121095b054015a
Jerry Cat <pzeus@163.com>
2026-09-15 00:33:08 +0800

    test: prevent e2e child log pipe backpressure

--- src/bin/e2e_bench.rs
+++ src/bin/e2e_bench.rs
@@ -90,7 +90,7 @@ fn spawn_rushway(
         .arg("-k")
         .arg(key)
         .arg("--log")
-        .arg("DEBUG");
+        .arg("ERROR");
     if allow_local_targets {
         cmd.arg("--no-block-local");
     }
